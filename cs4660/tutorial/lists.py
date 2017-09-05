66123f3244d625957e7ed12157063b1b023868fa
niivek <niivek@gmail.com>
2017-09-05 15:16:53 -0700

    lists.py completed

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,21 +1,31 @@
 """Lists defines simple list related operations"""
 
 def get_first_item(li):
-    """Return the first item from the list"""
+    return li[0]
     pass
 
 def get_last_item(li):
-    """Return the last item from the list"""
+    return li[len(li)-1]
     pass
 
 def get_second_and_third_items(li):
-    """Return second and third item from the list"""
+    return li[1:3]
     pass
 
 def get_sum(li):
-    """Return the sum of the list items"""
+    return sum(li)
     pass
 
 def get_avg(li):
-    """Returns the average of the list items"""
+    return sum(li)/(len(li)-1)
     pass
+
+print(get_first_item([1,2,3,4,5,6,7]))
+
+print(get_last_item([1,2,3,4,5,6,7]))
+
+print(get_second_and_third_items([1,2,3,4,5,6,7]))
+
+print(get_sum([1,2,3,4,5,6,7]))
+
+print (get_avg([1,2,3,4,5,6,7]))
