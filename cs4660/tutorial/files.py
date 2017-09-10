552b914d5313d9d0804a5daf435cc30bd6b27989
niivek <niivek@gmail.com>
2017-09-09 17:09:41 -0700

    Finally finished

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -9,30 +9,55 @@ class SimpleFile(object):
         dimension array (numbers)
         """
 
+        f = open(file_path)
+        for line in f:
+            self.numbers.append(line.strip().split(" "))
+
+
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        avg = 0
+        for x in self.numbers[line_number]:
+            avg += float(x)
+
+        avg = avg/len(self.numbers[line_number])
+        return float(avg)
+
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        lineMax = 0
+        for x in self.numbers[line_number]:
+            if x > lineMax:
+                lineMax = x
+
+        return int(lineMax)
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        lineMin = self.numbers[line_number][0]
+        for x in self.numbers[line_number]:
+            if x < lineMin:
+                lineMin = x
+
+        return int(lineMin)
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        lineSum = 0
+        for x in self.numbers[line_number]:
+            lineSum += int(x)
+
+        return int(lineSum)
