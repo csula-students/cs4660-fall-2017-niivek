6344b3fae5d66971a79365a941651ca1f1202922
Kevin Tran <niivek@gmail.com>
2017-10-07 08:33:51 -0700

    added hwk2

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,21 +1,21 @@
 """Lists defines simple list related operations"""
 
 def get_first_item(li):
-    return li[0]
+    """Return the first item from the list"""
     pass
 
 def get_last_item(li):
-    return li[len(li)-1]
+    """Return the last item from the list"""
     pass
 
 def get_second_and_third_items(li):
-    return li[1:3]
+    """Return second and third item from the list"""
     pass
 
 def get_sum(li):
-    return sum(li)
+    """Return the sum of the list items"""
     pass
 
 def get_avg(li):
-    return sum(li)/(len(li))
+    """Returns the average of the list items"""
     pass
